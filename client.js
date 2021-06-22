5c40f236ea5b082d4031021b57346251483e90e6
Kash <kserenitym@gmail.com>
2021-06-21 23:06:02 -0500

    idk how the pictures randomize, but they do?

--- client.js
+++ client.js
@@ -1,1 +1,28 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+let currentPerson = "";
+
+
+function onReady() {
+    console.log('Start!')
+    displayPictures();
+    
+}
+
+function displayPictures() {
+    console.log('in pictures')
+    for (let person of people.sort(function () { return 0.5 - Math.random() })) {
+        let url = $(`
+        <div class="image"><img src="https://github.com/${person.githubUsername}.png?size=250"
+        alt="Profile image of ${person.name}"></div>`);
+
+        url.data(person);
+        $('.photos').append(url);
+    }
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
